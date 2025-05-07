1b4c120b4fbd41feacb702583d005ab181409416
piotrmizerka <piotrmizerka@gmail.com>
2025-05-07 18:49:48 +0200

    2025.05.08 update

---
+++ 2025_04_24/hello_world.hs
@@ -0,0 +1,15 @@
+main :: IO ()
+main = putStrLn "Hello, world!"
+
+-- 1+2
+-- :t 1+2
+-- "fasfaf"++"dadd"
+-- :t "faff"++"faffff"
+
+-- Struktura drzewa BST
+-- data Tree a = Empty
+--            | Node a (Tree a) (Tree a)
+
+-- unwords ["Ala", "ma", "kota"] wyswietla ze spacjami pomiedzy
+
+-- https://learnyouahaskell.com/introduction
---
+++ 2025_05_08/task.md
@@ -0,0 +1,3 @@
+1. Write haskell code to check if a number is prime or not
+
+2. Generate Fibonacci numbers for a given 'n' in Haskell
